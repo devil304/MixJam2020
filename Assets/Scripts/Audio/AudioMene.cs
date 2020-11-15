40351f75d0ca4abd0e9e0a9a9a54cc31c4d29e50
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-11-15 19:18:49 +0100

    Release?

--- Assets/Scripts/Audio/AudioMene.cs
+++ Assets/Scripts/Audio/AudioMene.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class AudioMene : MonoBehaviour
 {
     [SerializeField] AudioSource HeadAct, HeadNext, AmbAct, AmbNext;
-    bool amb = true, next = false, switching = false;
-    [SerializeField] UnityEvent UE;
+    [HideInInspector] public bool amb = true, next = false, switching = false;
+    [SerializeField] UnityEvent UE, HOn, HOff;
     float HeadLastTime=0, AmbLastTime=0, HLTStamp, ALTStamp;
-
+    [HideInInspector] public bool disableSwitching = true, nextA = false;
+    [SerializeField] TextMeshProUGUI TMP;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !disableSwitching)
         {
             UE.Invoke();
         }
@@ -28,10 +30,93 @@ public class AudioMene : MonoBehaviour
 
     }
 
+    public void SwitchToH(float time)
+    {
+        if (!next && !switching && amb)
+        {
+            switching = true;
+            StartCoroutine(SHA(time));
+        }
+
+    }
+
+    public void SwitchToA(float time)
+    {
+        if (!next && !switching && !amb)
+        {
+            switching = true;
+            StartCoroutine(SHA(time));
+        }
+
+    }
+
+    public void setNextAMB(AudioClip AC)
+    {
+        AmbNext.clip = AC;
+        AmbNext.volume = 0;
+    }
+
+    public void setNextHead(AudioClip AC)
+    {
+        HeadNext.clip = AC;
+        HeadNext.volume = 0;
+    }
+
+    public void Snext(float Time)
+    {
+        StartCoroutine(NextIE(Time));
+    }
+
+    IEnumerator NextIE(float t)
+    {
+        disableSwitching = true;
+        if (amb)
+        {
+            AmbNext.volume = 0f;
+            AmbNext.enabled = true;
+            while (AmbNext.volume < 1f || AmbAct.volume > 0f)
+            {
+                AmbNext.volume += (1f / t) * Time.deltaTime;
+                AmbNext.volume = AmbNext.volume > 1f ? 1f : AmbNext.volume;
+                AmbAct.volume -= (1f / t) * Time.deltaTime;
+                AmbAct.volume = AmbAct.volume > 1f ? 1f : AmbAct.volume;
+                yield return null;
+            }
+            AmbAct.clip = AmbNext.clip;
+            AmbAct.time = AmbNext.time;
+            AmbAct.volume = AmbNext.volume;
+            AmbAct.Play();
+            //AmbAct.enabled = true;
+            AmbNext.enabled = false;
+        }
+        else
+        {
+            HeadNext.volume = 0f;
+            HeadNext.enabled = true;
+            while (HeadNext.volume < 1f || HeadAct.volume > 0f)
+            {
+                HeadNext.volume += (1f / t) * Time.deltaTime;
+                HeadNext.volume = HeadNext.volume > 1f ? 1f : HeadNext.volume;
+                HeadAct.volume -= (1f / t) * Time.deltaTime;
+                HeadAct.volume = HeadAct.volume > 1f ? 1f : HeadAct.volume;
+                yield return null;
+            }
+            HeadAct.clip = HeadNext.clip;
+            HeadAct.time = HeadNext.time;
+            HeadAct.volume = HeadNext.volume;
+            HeadAct.Play();
+            //HeadAct.enabled = true;
+            HeadNext.enabled = false;
+        }
+        disableSwitching = false;
+    }
+
     IEnumerator SHA(float t)
     {
         if (amb)
         {
+            TMP.text = "Headphones: ON<br>[Press SPACE to switch]";
+            HOn.Invoke();
             HeadAct.volume = 0f;
             HeadAct.enabled = true;
             if (HeadLastTime != 0)
@@ -52,6 +137,8 @@ public class AudioMene : MonoBehaviour
         }
         else
         {
+            TMP.text = "Headphones: OFF<br>[Press SPACE to switch]";
+            HOff.Invoke();
             AmbAct.volume = 0f;
             AmbAct.enabled = true;
             if (AmbLastTime != 0)
--- Assets/Scripts/Flickering.cs
+++ Assets/Scripts/Flickering.cs
@@ -2,13 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.VFX;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class Flickering : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera CVC;
-    [SerializeField] Animator anim;
+    [SerializeField] Animator animL;
+    [SerializeField] VisualEffect VE;
     [SerializeField] FirstPersonController FPC;
+    [SerializeField] AudioSource AS;
+    [SerializeField] Light l;
+    [SerializeField] AudioMene AM;
+    [SerializeField] UnityEvent StartF, StopF;
     float OldWalk, Xsen, Ysen;
     public void starflick(float time)
     {
@@ -17,18 +24,40 @@ public class Flickering : MonoBehaviour
         Ysen = FPC.m_MouseLook.YSensitivity;
         StartCoroutine(flicker(time));
     }
+
+    public void chamgeAnim(RuntimeAnimatorController RAC)
+    {
+        animL.runtimeAnimatorController = RAC;
+    }
     IEnumerator flicker(float t)
     {
+        FPC.m_WalkSpeed = 0;
+        FPC.m_MouseLook.XSensitivity = 0;
+        FPC.m_MouseLook.YSensitivity = 0;
+        while (AM.switching)
+        {
+            yield return null;
+        }
+        AM.disableSwitching = true;
+        StartF.Invoke();
+        bool tmp = AM.amb;
+        AM.SwitchToA(0.75f);
+        if(!tmp)
+            yield return new WaitForSecondsRealtime(0.5f);
+        AS.PlayOneShot(AS.clip);
         CVC.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 1;
-        //anim.SetBool("Fall",true);
-        FPC.m_WalkSpeed=0;
-        FPC.m_MouseLook.XSensitivity=0;
-        FPC.m_MouseLook.YSensitivity=0;
+        VE.enabled = true;
+        animL.enabled = true;
+
         yield return new WaitForSecondsRealtime(t);
-        //anim.SetBool("Fall", false);
+        VE.enabled = false;
+        animL.enabled = false;
+        l.enabled = true;
         CVC.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
+        StopF.Invoke();
         FPC.m_WalkSpeed = OldWalk;
         FPC.m_MouseLook.XSensitivity = Xsen;
         FPC.m_MouseLook.YSensitivity = Ysen;
+        AM.disableSwitching = false;
     }
 }
